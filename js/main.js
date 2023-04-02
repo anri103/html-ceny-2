327d3fcdc933495a62bd8c0d85c867ecc883d004
anri103 <ianrianri@gmail.com>
2023-04-03 01:43:10 +0300

    add content

--- js/main.js
+++ js/main.js
@@ -1,60 +1,74 @@
-/*
-Theme: Smeshnie Ceny
-Author: Andrey Galkin
-Version: 2.0
-*/
+window.addEventListener('DOMContentLoaded', event => { });
 
-//
-// Scripts
-// 
+window.onscroll = function () {
+    const windowScroll = window.scrollY;
+    const fixedMenu = document.querySelector('.header-fixed');
+    if (windowScroll > 500) {
+        fixedMenu.classList.add('show');
+    }
+    else {
+        fixedMenu.classList.remove('show');
+    }
+}
 
-window.addEventListener('DOMContentLoaded', event => {
+const blogSwiper = new Swiper('.blogSwiper', {
+    slidesPerView: 1.05,
+    loop: true,
+    grabCursor: true,
+    autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '#blogSwiper-next',
+        prevEl: '#blogSwiper-prev',
+    },
+    breakpoints: {
+        768: {
+            slidesPerView: 2,
+        },
+        992: {
+            slidesPerView: 3,
+        },
+        1200: {
+            slidesPerView: 4,
+        },
+    },
+});
 
-    const blogSwiper = new Swiper('.blogSwiper', {
-        slidesPerView: 1.05,
-        loop: true,
-        autoplay: {
-            delay: 3500,
-            disableOnInteraction: false,
+const magazinSwiper = new Swiper('.magazinSwiper', {
+    slidesPerView: 1.05,
+    loop: true,
+    grabCursor: true,
+    autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '#magazinSwiper-next',
+        prevEl: '#magazinSwiper-prev',
+    },
+    breakpoints: {
+        768: {
+            slidesPerView: 2,
         },
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
+        992: {
+            slidesPerView: 3,
         },
-        navigation: {
-            nextEl: '#blogSwiper-next',
-            prevEl: '#blogSwiper-prev',
+        1200: {
+            slidesPerView: 4,
         },
-        breakpoints: {
-            768: {
-                slidesPerView: 3,
-            },
-        },
-    });
+    },
+});
 
-    const magazinSwiper = new Swiper('.magazinSwiper', {
-        slidesPerView: 1.05,
-        loop: true,
-        autoplay: {
-            delay: 3500,
-            disableOnInteraction: false,
-        },
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-        navigation: {
-            nextEl: '#magazinSwiper-next',
-            prevEl: '#magazinSwiper-prev',
-        },
-        breakpoints: {
-            768: {
-                slidesPerView: 3,
-            },
-            992: {
-                slidesPerView: 4,
-            },
-        },
-    });
-
+Fancybox.bind("[data-fancybox]", {
+    // Your custom options
 });
